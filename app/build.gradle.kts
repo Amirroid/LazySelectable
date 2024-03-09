10329b986ac3aef1968d007a8d7a269965c4d884
Amirreza Gholami <jv233114@gmail.com>
2024-03-09 20:39:47 +0330

    change plugin versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
-    packaging {
+    packagingOptions {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
